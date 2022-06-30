4527454d8bfddeb1901a4297816fe044241db42c
John Doe <soumya.antipeta@rampgroup.com>
2022-06-30 16:45:42 +0530

    Canvas

---
+++ app/src/main/java/com/example/canvas/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.canvas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        val myCanvas = MyCanvas(this)
+        myCanvas.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        setContentView(myCanvas)
+    }
+
+}
